6f9ca99e3ba926ad078950e73281da2fc3e26d15
peter <113376712+Ptahakash@users.noreply.github.com>
2024-03-14 18:35:14 +0300

    second commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+</script>
+
+<template>
+  <div class="nav">
+    <RouterLink class="nav-item" to="/">Home</RouterLink>
+    <RouterLink class="nav-item" to="/about">About</RouterLink>
+    <RouterLink class="nav-item" to="">Services</RouterLink>
+    <RouterLink class="nav-item" to="">Contact us</RouterLink>
+    <RouterLink class="nav-item" to="/login">Login</RouterLink>
+    <RouterLink class="nav-item" to="/signup">Signup</RouterLink>
+  </div>
+  <router-view></router-view>
+  
+</template>
+
+<style scoped>
+  .nav{
+    background-color: green;
+    padding: 14px 28px;
+  }
+
+  .nav .nav-item{
+    text-decoration: none;
+    padding: 0px 28px;
+    color: white;
+    font-weight: bold;
+    font-family: 'Courier New', Courier, monospace;
+  }
+</style>
+
---
+++ src/views/SignupView.vue
@@ -0,0 +1,55 @@
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+const username = ref('john')
+const email = ref('john@gmail.com')
+const password = ref('john123')
+
+const signup = async () => {
+    console.log(`${username.value} ${email.value} ${password.value}`);
+    // Save the value
+    await axios.post('http://127.0.0.1:8000/api/signup', {username: username.value, email: email.value, password: password.value});
+}
+
+</script>
+
+<template>
+    <div class="signup-form">
+        <h1>Signup Here</h1>
+        <form @submit.prevent="signup">
+            <div class="form-group">
+                <input class="form-input" type="text" placeholder="Enter username" v-model="username">
+            </div>
+            <div class="form-group">
+                <input class="form-input" type="email" placeholder="Enter email" v-model="email">
+            </div>
+            <div class="form-group">
+                <input class="form-input" type="password" placeholder="Enter password" v-model="password">
+            </div>
+            <div class="form-group">
+                <input class="form-input" type="submit" value="Signup">
+            </div>
+        </form>
+        <p>{{username}}</p>
+    </div>
+</template>
+
+<style scoped>
+    .signup-form{
+        background-color: rgb(132, 132, 146);
+        width: 50%;
+        padding: 1%;
+        border-radius: 10px;
+    }
+    .signup-form .form-group{
+        margin-bottom: 1%;
+    }
+
+    .signup-form .form-input{
+        border-radius: 5px;
+        border-style: none;
+        padding: 5px 28px;
+    }
+</style>
